85157d244aaa448037f300bc1ad5610149d986a0
Sanjaee <afrizaahmad18@gmail.com>
2023-12-10 15:25:47 +0700

    Revisi1

--- src/Components/Popup/IconService.jsx
+++ src/Components/Popup/IconService.jsx
@@ -1,12 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Styles/IconService.css"; // Import file CSS
 
 const ServiceIcon = () => {
   return (
-    <div className="service-icon">
+    <Link to="/Service" className="service-icon">
       <img src="/technical-support.png" alt="Service Icon" />
-      <a href=""></a>
-    </div>
+    </Link>
   );
 };
 
