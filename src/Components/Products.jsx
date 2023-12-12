3fd37f8c51d01c4636e0e43701a91466edfd3177
Sanjaee <afrizaahmad18@gmail.com>
2023-12-12 19:58:32 +0700

    Revis7

--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -6,10 +6,12 @@ import "../Styles/Products.css";
 import { Link } from "react-router-dom";
 import { faStar, faMapMarker } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Skeleton from "react-loading-skeleton";
 
 const Products = () => {
   const [kontrakan, setKontrakan] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -19,7 +21,12 @@ const Products = () => {
         id: doc.id,
         ...doc.data(),
       }));
-      setKontrakan(kontrakanData);
+
+      // Simulate a 1.5-second loading delay
+      setTimeout(() => {
+        setKontrakan(kontrakanData);
+        setLoading(false);
+      }, 900);
 
       // Set up a real-time listener for updates
       const unsubscribe = onSnapshot(kontrakanCollection, (querySnapshot) => {
@@ -74,51 +81,57 @@ const Products = () => {
         </div>
 
         <div className="grid grid-cols-2 sm:grid-cols-4 gap-1">
-          {filteredKontrakan.map((item) => (
-            <Link
-              to={`/DetailProducts/${item.id}`}
-              key={item.id}
-              className="store-card"
-            >
-              <div className="store-card-inner">
-                <img
-                  src={item.image}
-                  alt={item.name}
-                  className="store-card-image"
-                />
-
-                <div className="store-card-details">
-                  <div className="store-card-title-wrapper">
-                    <h2 className="store-card-title">{item.name}</h2>
+          {loading
+            ? filteredKontrakan.map((item, index) => (
+                <div key={index} className="store-card">
+                  <div className="skeleton-container">
+                    <Skeleton height={200} width="100%" />
                   </div>
-
-                  <p className="store-card-description">{item.title}</p>
-                  <div
-                    className={`status-dot ${
-                      item.status === "TERSEDIA" ? "green-dot" : "red-dot"
-                    }`}
-                  ></div>
-                  <p className="store-card-description">{item.status}</p>
-
-                  <div className="location">
-                    <FontAwesomeIcon icon={faMapMarker} />{" "}
-                    <p className="lokasi">{item.location}</p>
-                  </div>
-                  <p className="store-card-price">
-                    {formatCurrency(item.price)}
-                  </p>
-                  <div className="rating">
-                    {Array.from({ length: item.rating }, (_, index) => (
-                      <FontAwesomeIcon icon={faStar} key={index} />
-                    ))}
-                  </div>
-                  <p className="store-card-description">{item.date}</p>
                 </div>
-              </div>
-            </Link>
-          ))}
+              ))
+            : filteredKontrakan.map((item) => (
+                <Link
+                  to={`/DetailProducts/${item.id}`}
+                  key={item.id}
+                  className="store-card"
+                >
+                  <div className="store-card-inner">
+                    <img
+                      src={item.image}
+                      alt={item.name}
+                      className="store-card-image"
+                    />
 
-          {/* Tampilkan jumlah card di samping kanan */}
+                    <div className="store-card-details">
+                      <div className="store-card-title-wrapper">
+                        <h2 className="store-card-title">{item.name}</h2>
+                      </div>
+
+                      <p className="store-card-description">{item.title}</p>
+                      <div
+                        className={`status-dot ${
+                          item.status === "TERSEDIA" ? "green-dot" : "red-dot"
+                        }`}
+                      ></div>
+                      <p className="store-card-description">{item.status}</p>
+
+                      <div className="location">
+                        <FontAwesomeIcon icon={faMapMarker} />{" "}
+                        <p className="lokasi">{item.location}</p>
+                      </div>
+                      <p className="store-card-price">
+                        {formatCurrency(item.price)}
+                      </p>
+                      <div className="rating">
+                        {Array.from({ length: item.rating }, (_, index) => (
+                          <FontAwesomeIcon icon={faStar} key={index} />
+                        ))}
+                      </div>
+                      <p className="store-card-description">{item.date}</p>
+                    </div>
+                  </div>
+                </Link>
+              ))}
         </div>
       </div>
     </section>
