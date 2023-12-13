72e6a669ae205db41982806f51a56613f9408dd9
Sanjaee <afrizaahmad18@gmail.com>
2023-12-13 12:57:25 +0700

    Revis8

--- src/Pages/Service.jsx
+++ src/Pages/Service.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-
-import { Link } from "react-router-dom";
+import { Navbar } from "../Components/Navbar";
+import { ToastContainer, toast } from "react-toastify";
 
 const Service = () => {
   const [formData, setFormData] = useState({
@@ -44,67 +44,79 @@ const Service = () => {
         catatan: "",
       });
     }, 2000);
+    toast.success("Pesan Terkirim Ke Admin");
   };
 
   return (
-    <div className="flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
-      <div className="w-full max-w-md">
-        <h1 className="text-2xl font-bold mb-4">Hubungi Kami</h1>
-        <div className="space-y-4">
+    <div>
+      <Navbar />
+      <div className="flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+        <div className="w-full max-w-md">
           <div>
-            <label className="block text-sm font-medium">NAMA</label>
-            <input
-              type="text"
-              className="w-full px-4 py-2 border rounded-md"
-              name="namaAkun"
-              onChange={handleChange}
-              value={formData.namaAkun}
-              placeholder="NAMA"
+            <img
+              className="mx-auto py-2 px-4  w-1/2 mt-10"
+              src="/service.jpg"
+              alt=""
             />
           </div>
-          <div>
-            <label className="block text-sm font-medium">NOMOR TELEPON</label>
-            <input
-              type="number"
-              className="w-full px-4 py-2 border rounded-md"
-              name="nomorOrderan"
-              onChange={handleChange}
-              value={formData.nomorOrderan}
-              placeholder="+62812345678"
-            />
-          </div>
-          <div>
-            <label className="block text-sm font-medium">EMAIL</label>
-            <input
-              type="email"
-              className="w-full px-4 py-2 border rounded-md"
-              name="email"
-              onChange={handleChange}
-              value={formData.email}
-              placeholder="email@gmail.com"
-            />
-          </div>
-          <div>
-            <label className="block text-sm font-medium">
-              CATATAN / PERIHAL
-            </label>
+          <h1 className="text-2xl font-bold mb-4 mt-10">Hubungi Kami</h1>
+          <div className="space-y-4">
             <div>
-              <textarea
+              <label className="block text-sm font-medium">NAMA</label>
+              <input
+                type="text"
                 className="w-full px-4 py-2 border rounded-md"
-                name="catatan"
-                placeholder="Opsional"
+                name="namaAkun"
                 onChange={handleChange}
-                value={formData.catatan}
-                rows="4"
-              ></textarea>
-              <button
-                className="bg-blue-500 text-white px-4 py-2 rounded-md mt-4 w-full hover:bg-blue-600"
-                id="tombol"
-                onClick={handleWhatsApp}
-              >
-                <span>SEND</span>
-              </button>
+                value={formData.namaAkun}
+                placeholder="NAMA"
+              />
             </div>
+            <div>
+              <label className="block text-sm font-medium">NOMOR TELEPON</label>
+              <input
+                type="number"
+                className="w-full px-4 py-2 border rounded-md"
+                name="nomorOrderan"
+                onChange={handleChange}
+                value={formData.nomorOrderan}
+                placeholder="+62812345678"
+              />
+            </div>
+            <div>
+              <label className="block text-sm font-medium">EMAIL</label>
+              <input
+                type="email"
+                className="w-full px-4 py-2 border rounded-md"
+                name="email"
+                onChange={handleChange}
+                value={formData.email}
+                placeholder="email@gmail.com"
+              />
+            </div>
+            <div>
+              <label className="block text-sm font-medium">
+                CATATAN / PERIHAL
+              </label>
+              <div>
+                <textarea
+                  className="w-full px-4 py-2 border rounded-md"
+                  name="catatan"
+                  placeholder="Opsional"
+                  onChange={handleChange}
+                  value={formData.catatan}
+                  rows="4"
+                ></textarea>
+                <button
+                  className="bg-blue-500 text-white px-4 py-2 rounded-md mt-4 w-full hover:bg-blue-600"
+                  id="tombol"
+                  onClick={handleWhatsApp}
+                >
+                  <span>SEND</span>
+                </button>
+              </div>
+            </div>
+            <ToastContainer position="top-right" autoClose={2000} />
           </div>
         </div>
       </div>
